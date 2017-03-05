633f0783c23435e1283cf6c4cc7cf1a5e78090bd
Naomi Mine <naomi.mine@gmail.com>
2017-03-05 12:41:07 -0600

    able to log in

--- server/app.js
+++ server/app.js
@@ -18,11 +18,29 @@ app.get('/', function(request, response){
 app.get('/register', function(request, response){
 	response.render('register', {})
 })
+app.get('/login', function(request, response){
+	response.render('login', {})
+})
 
-app.post('/register', function(request, response){
+
+app.post('/users', function(request, response){
 	users.push({username: request.body.username, password: request.body.password})
 	console.log(users);
 	response.render('register');
+})
+
+app.post('/loggin', function(request, response){
+	console.log(request.body.username);
+	console.log(request.body.password);
+	for(i=0; i<users.length; i++){
+		if(users[i].username === request.body.username){
+			if(users[i].password === request.body.password){
+				response.send("You're in!")
+			} else {
+				response.send("Failed to log in");
+			}
+		}
+	}
 })
 
 
